ab1a234e11f43e8c82d8f280edb87f826b9ae193
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-07-14 10:27:26 +0200

    Add config file

--- config.go
+++ config.go
@@ -1,7 +1,5 @@
 //**********************************************************
 //
-// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
-//
 // This file is part of lexoffice.
 // All code may be used. Feel free and maybe code something better.
 //
@@ -10,3 +8,49 @@
 //**********************************************************
 
 package lexoffice
+
+import (
+	"bytes"
+	"net/http"
+)
+
+const (
+	resourceUrl = "https://api.lexoffice.io"
+)
+
+// Request is to define the request data
+type Request struct {
+	Path, Method, Token string
+	Body                []byte
+}
+
+// Send is to send a new request
+func (r *Request) Send() (*http.Response, error) {
+
+	// Set url
+	url := resourceUrl + r.Path
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(r.Method, url, bytes.NewBuffer(r.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Define header
+	request.Header.Set("Authorization", "Bearer "+r.Token)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/json")
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
