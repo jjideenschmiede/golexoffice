b8c151e981d81ac1e9e96ee0f94bcbd66dff3da3
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-09-25 11:37:55 +0200

    Add function to upload a file

---
+++ files.go
@@ -0,0 +1,90 @@
+//********************************************************************************************************************//
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of golexoffice.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package golexoffice
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+)
+
+// FileReturn is to decode json data
+type FileReturn struct {
+	Id string `json:"id"`
+}
+
+// AddFile is to upload a file
+func AddFile(filePath string, token string) (FileReturn, error) {
+
+	// Create form data
+	body := &bytes.Buffer{}
+
+	// Create writer
+	writer := multipart.NewWriter(body)
+
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Close file after function ends
+	defer file.Close()
+
+	// Create body data
+	filePart, err := writer.CreateFormFile("file", filepath.Base(filePath))
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Copy form & file
+	_, err = io.Copy(filePart, file)
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Create text part
+	_ = writer.WriteField("type", "voucher")
+
+	// Close writer
+	err = writer.Close()
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Set config for new request
+	c := Config{"/v1/files/", "POST", token, writer.FormDataContentType(), body}
+
+	// Send request
+	response, err := c.Send()
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode FileReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return FileReturn{}, err
+	}
+
+	// Return data
+	return decode, nil
+
+}
